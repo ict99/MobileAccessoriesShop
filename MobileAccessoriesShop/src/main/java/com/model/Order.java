8c52a3ee30a11025ca4b077de1c49e97f882a786
ict99 <107979971+ict99@users.noreply.github.com>
2022-07-13 20:52:28 +0530

    Add files via upload

---
+++ MobileAccessoriesShop/src/main/java/com/model/Order.java
@@ -0,0 +1,36 @@
+package com.model;
+
+//pojo class with required attributes,getters and setters 
+public class Order {
+	
+	private String customerName;
+	private String contactNumber;
+	private String productName;
+	private int quantity;
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+}
